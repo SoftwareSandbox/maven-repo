7b392d06455f7f2df0436b8aaa0e69e34f9b1634
mdeswer1 <mike.de.swert@staff.telenet.be>
2015-11-06 14:52:51 +0100

    edited shell for snapshot + deleted release of atom-feed-core and metrics

--- mvn-install.sh
+++ mvn-install.sh
@@ -1,7 +1,7 @@
 mvn install:install-file \
  -DgroupId=be.swsb.atom-feed \
  -DartifactId=atom-feed-core \
- -Dversion=0.0.1 \
+ -Dversion=0.0.1-SNAPSHOT \
  -Dpackaging=jar \
  -Dfile=atom-feed-core-0.0.1-SNAPSHOT.jar \
  -DlocalRepositoryPath=. 
